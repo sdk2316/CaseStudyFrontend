7ffaa05fe2cdfb66fddfbc39d84fbdd86bf389ca
23dgupta <durgeshkumar91g@gmail.com>
2022-07-01 19:06:02 +0530

    project completed

--- flightbookingapp/src/app/addflight.service.ts
+++ flightbookingapp/src/app/addflight.service.ts
@@ -8,73 +8,71 @@ import { Bookflight } from './bookflight';
   providedIn: 'root'
 })
 export class AddflightService {
-  private basrURL="http://localhost:9094/allFlights";
+  //:9094/allFlights
+  baseURL="http://ec2-35-90-107-69.us-west-2.compute.amazonaws.com"
 
-  private ubaseURL="http://localhost:9090/user/api/v1.0"
+ // private baseURLU="http://ec2-35-88-83-206.us-west-2.compute.amazonaws.com"
 
   constructor(private httpClient:HttpClient) { }
 
 // user end point
 searchFlight(fromPlace:string,toPlace:string,startDate:any):Observable<AddFlight[]>{
-  return this.httpClient.get<AddFlight[]>("http://localhost:9090/user/api/v1.0/"+"search/"+fromPlace+"/"+toPlace+"/"+startDate);
+  return this.httpClient.get<AddFlight[]>(`${this.baseURL}:9090/user/api/v1.0/search/${fromPlace}/${toPlace}/${startDate}`);
 }
 
 bookFlight(bookflight:Bookflight):Observable<Object>{
-  return this.httpClient.post(`http://localhost:9090/user/api/v1.0/bookFlight`,bookflight,{
+  return this.httpClient.post(`${this.baseURL}:9090/user/api/v1.0/bookFlight`,bookflight,{
     responseType:'text'
   })
 }
 
-
 getAllBookedFlights():Observable<Bookflight[]>{
-  return this.httpClient.get<Bookflight[]>('http://localhost:9090/user/api/v1.0/getAllBookFlight')
+  return this.httpClient.get<Bookflight[]>(`${this.baseURL}:9090/user/api/v1.0/getAllBookFlight`)
 }
 
 deleteBookedFlight(pnr:number):Observable<Object>{
-  return this.httpClient.delete(`http://localhost:9090/user/api/v1.0/getCancelByPnr/${pnr}`,{
+  return this.httpClient.delete(`${this.baseURL}:9090/user/api/v1.0/getCancelByPnr/${pnr}`,{
     responseType:'text'
   });
 }
 
-
-
 getBookFlightByEmail(email:string):Observable<Bookflight[]>{
-  return this.httpClient.get<Bookflight[]>(`http://localhost:9090/user/api/v1.0/getFlightByEmailId/${email}`
+  return this.httpClient.get<Bookflight[]>(`${this.baseURL}:9090/user/api/v1.0/getFlightByEmailId/${email}`
 );
 }
 
 getBookFlightPnr(pnr:number):Observable<Bookflight[]>{
-  return this.httpClient.get<Bookflight[]>(`http://localhost:9090/user/api/v1.0/getFlightByPnr/${pnr}`);
+  return this.httpClient.get<Bookflight[]>(`${this.baseURL}:9090/user/api/v1.0/getFlightByPnr/${pnr}`);
 }
 
 // above all user end point
 //GET /user/api/v1.0/search/{fromPlace}/{toPlace}/{startDate}
 
   getAllFlightsList():Observable<AddFlight[]>{
-    return this.httpClient.get<AddFlight[]>(`${this.basrURL}`);
+    return this.httpClient.get<AddFlight[]>(`${this.baseURL}:9094/allFlights`);
   }
 
   // for postmapping
   addFlight( addFlight:AddFlight):Observable<Object>{
    // return this.httpClient.post(`${this.basrURL}`,addFlight)
-    return this.httpClient.post(`http://localhost:9094/addFlight`,addFlight
+    return this.httpClient.post(`${this.baseURL}:9094/addFlight`,addFlight
     )
 
   }
 
   getSearchFlightByflightNumber(flightNumber:number):Observable<AddFlight>{
-    return this.httpClient.get<AddFlight>(`http://localhost:9094/getFlightsByNumber/${flightNumber}`);
+    return this.httpClient.get<AddFlight>(`${this.baseURL}:9094/getFlightsByNumber/${flightNumber}`);
   }
 
   updateFlight(flightNumber:number,addFlight:AddFlight):Observable<Object>{
-    return this.httpClient.put(`http://localhost:9094/updateFlight/${flightNumber}`,addFlight);
+    return this.httpClient.put(`${this.baseURL}:9094/updateFlight/${flightNumber}`,addFlight);
   }
 
   deleteFlight(flightNumber:number):Observable<Object>{
-    return this.httpClient.delete(`http://localhost:9094/deleteFlight/${flightNumber}`);
+    return this.httpClient.delete(`${this.baseURL}:9094/deleteFlight/${flightNumber}`);
   }
 
   getSearchFlightByflightName(flightName:string):Observable<AddFlight>{
-    return this.httpClient.get<AddFlight>(` http://localhost:9094/flightsByName/${flightName}`);
+    return this.httpClient.get<AddFlight>(`${this.baseURL}:9094/flightsByName/${flightName}`);
   }
 }
